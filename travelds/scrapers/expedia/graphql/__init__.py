9338b2fb4f2b46529aef4df33a579d300d9062e7
Jakub Kot <xnetcat.dev@gmail.com>
2021-11-14 02:42:18 +0100

    added __init__ for scrapers

---
+++ travelds/scrapers/expedia/graphql/__init__.py
@@ -0,0 +1,2 @@
+from travelds.scrapers.expedia.graphql.listing import LISTING_QUERY
+from travelds.scrapers.expedia.graphql.search import SEARCH_QUERY
---
+++ travelds/scrapers/hotels/graphql/__init__.py
@@ -0,0 +1,2 @@
+from travelds.scrapers.hotels.graphql.listing import LISTING_QUERY
+from travelds.scrapers.hotels.graphql.search import SEARCH_QUERY
