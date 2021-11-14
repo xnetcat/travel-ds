08b87593bc9f0a20a9a172a16a4adc60aefacb48
Jakub Kot <xnetcat.dev@gmail.com>
2021-11-14 11:49:07 +0100

    added missing import for agoda

--- travelds/etl/models/__init__.py
+++ travelds/etl/models/__init__.py
@@ -1,2 +1,3 @@
 from travelds.etl.models.expedia import ExpediaListing, ExpediaPrice
 from travelds.etl.models.hotels import HotelsListing, HotelsPrice
+from travelds.etl.models.agoda import AgodaListing, AgodaPrice
